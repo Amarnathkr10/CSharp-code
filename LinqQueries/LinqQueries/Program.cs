1924cd6a4a790df31bd607c988061b7edfc5e706
Amarnath Kumar <amarnathhkr@gmail.com>
2025-08-06 18:32:27 +0530

    adding the code into csharp-code

---
+++ LinqQueries/LinqQueries/Program.cs
@@ -0,0 +1,58 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//namespace LINQDemo
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            List<int> integerList = new List<int>()
+//            {
+//                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+//            };
+
+//            var QuerySyntax = integerList.Where(obj => obj > 5).ToList();
+
+//            Console.WriteLine($"{QuerySyntax}");
+
+//            foreach (var item in QuerySyntax)
+//            {
+//                Console.Write(item + " ");
+//            }
+
+//            Console.ReadKey();
+//        }
+//    }
+//}
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> integerList = new List<int>()
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+            };
+
+            IEnumerable<int> QuerySyntax = from obj in integerList
+                                           where obj > 5
+                                           select obj;
+
+            Console.WriteLine(QuerySyntax);
+
+            foreach (var item in QuerySyntax)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
